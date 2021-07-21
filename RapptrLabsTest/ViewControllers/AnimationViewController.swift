5386d8274279721286b49a5ee218cbb254e29025
vivekptnk <vivekpattanaik100@gmail.com>
2021-07-20 22:20:09 -0400

    Bonus : Resolved #5 Added confetti animation

--- RapptrLabsTest/ViewControllers/AnimationViewController.swift
+++ RapptrLabsTest/ViewControllers/AnimationViewController.swift
@@ -6,30 +6,45 @@
 //
 
 import UIKit
+import ConfettiView
 
 class AnimationViewController: UIViewController {
-    
+
     // Declare the image embedded view and fadeInButton
     @IBOutlet weak var rapptrLabsLogo: UIImageView!
     @IBOutlet weak var fadeInButton: UIButton!
     @IBOutlet weak var fadeOutButton: UIButton!
+    @IBOutlet weak var confettiTimeButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .light // changing UI style to Light mode
         
         // stroke of the button
+        confettiTimeButton.layer.cornerRadius = 8
         fadeOutButton.layer.borderWidth = 1
         fadeOutButton.layer.borderColor = #colorLiteral(red: 0.05490196078, green: 0.3607843137, blue: 0.537254902, alpha: 1)
         
-        // setting initial opacity of the logo to be zero
-        rapptrLabsLogo.alpha = 0
         
     }
     
+    @IBAction func confettiTimeButtonPressed(_ sender: UIButton) {
+        
+        rapptrLabsLogo.alpha = 0
+        let confetti = ConfettiView() //
+        self.view.addSubview(confetti)
+        confetti.emit(with: [
+            .text("🙍🏻‍♂️"),
+            .text("📱"),
+            .text("💻"),
+            .text("🧑🏻‍💻"),
+            .image(UIImage(named: "AppIcon")!, nil)
+        ])
+    }
+    
+    
     // fade in button action
     @IBAction func fadeInButtonPressed(_ sender: UIButton) {
-        
         // add the image to a subview to conform to the extension of fadeIn animation check Extensions.swift for more
         self.view.addSubview(rapptrLabsLogo) // add subview
         rapptrLabsLogo.fadeIn() // simulate the fade in
